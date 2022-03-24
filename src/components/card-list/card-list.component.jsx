2e680b004f0e973dc8ed93d48c81299f555d3648
William Fontanez <william.r.fontanez@gmail.com>
2022-03-24 09:13:20 -0500

    Created card-list and card style sheets
    Styled the cards-list and cards components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,29 @@
 import { Component } from "react";
 
+import './card-list.style.css';
+import './card.style.css';
+
 class CardList extends Component {
 
     render() {
         const {monsters} = this.props;
         return (
-            <div>
-                {monsters.map((monster) => (
-                <h1 key={monster.id}>{monster.name}</h1>
-                ))} 
+            <div className='card-list'>
+                {monsters.map((monster) => {
+                    const{name, email, id} = monster;
+                    return(
+                    <div className = 'card-container' key={id}>
+                        <img 
+                            alt= {`monster ${name}`} 
+                            src={`https://robohash.org/${id}?set=set4&size=190x180`}
+                        />
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                    );
+                })} 
             </div>
-        )
+        );
     }
 }
 
